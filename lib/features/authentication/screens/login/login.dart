6d1771d7947781320db35c6760e4cd46151c59cc
Emmanuel Mbah <emmanuelmbah91@gmail.com>
2024-12-22 00:32:23 +0100

    update

--- lib/features/authentication/screens/login/login.dart
+++ lib/features/authentication/screens/login/login.dart
@@ -1,18 +1,13 @@
-import 'package:ecommerce_app/app.dart';
 import 'package:ecommerce_app/common/styles/spacing_styles.dart';
 import 'package:ecommerce_app/common/widgets_login_signup/social_buttons.dart';
-import 'package:ecommerce_app/features/authentication/controllers.onboarding/onboarding_controller.dart';
 import 'package:ecommerce_app/features/authentication/screens/login/widgets/login_form.dart';
 import 'package:ecommerce_app/features/authentication/screens/login/widgets/login_header.dart';
 import 'package:ecommerce_app/utils/constants/colors.dart';
-import 'package:ecommerce_app/utils/constants/image_strings.dart';
 import 'package:ecommerce_app/utils/constants/sizes.dart';
 import 'package:ecommerce_app/utils/constants/text_strings.dart';
 import 'package:ecommerce_app/utils/helpers/helper_functions.dart';
-import 'package:ecommerce_app/utils/theme/widget_themes/text_field_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:iconsax/iconsax.dart';
 
 class LoginSCreen extends StatelessWidget {
   const LoginSCreen({super.key});
