a73cde1bea51a616519910b36e7afb2aaf4e08e8
Emmanuel Mbah <emmanuelmbah91@gmail.com>
2024-12-30 14:32:51 +0100

    uopdate

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -2,134 +2,51 @@ import 'package:ecommerce_app/common/widgets/custon_shapes/containers/primary_he
 import 'package:ecommerce_app/common/widgets/custon_shapes/containers/search_container.dart';
 import 'package:ecommerce_app/common/widgets/texts/section_heading.dart';
 import 'package:ecommerce_app/features/shop/screens/home/widgets/home_appbar.dart';
+import 'package:ecommerce_app/features/shop/screens/home/widgets/home_categories.dart';
 import 'package:ecommerce_app/utils/constants/colors.dart';
-import 'package:ecommerce_app/utils/constants/image_strings.dart';
 import 'package:ecommerce_app/utils/constants/sizes.dart';
-import 'package:ecommerce_app/utils/device/device_utility.dart';
-import 'package:ecommerce_app/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
+            //header
             PrimaryHeaderContainer(
               child: Column(
                 children: [
                   /// AppBar
-                  const HomeAppbar(),
+                  HomeAppbar(),
 
-                  const SizedBox(
-                    height: TSizes.spaceBtwSections,
-                  ),
+                  SizedBox(height: TSizes.spaceBtwSections),
 
                   /// SearchBar
-                  const SearchContainer(
-                    text: 'Search in Store',
-                  ),
-                  const SizedBox(
-                    height: TSizes.spaceBtwSections,
-                  ),
+                  SearchContainer(text: 'Search in Store'),
+                  SizedBox(height: TSizes.spaceBtwSections),
 
-                  /// Categories
+                  /// all categories
                   Padding(
-                    padding: const EdgeInsets.only(left: TSizes.defaultSpace),
+                    padding: EdgeInsets.only(left: TSizes.defaultSpace),
                     child: Column(
                       children: [
-                        const SectionHeading(
-                          title: 'Popular Categories',
-                          showActionButton: false,
-                          textColor: TColors.white,
-                        ),
-                        const SizedBox(
-                          height: TSizes.spaceBtwItems,
-                        ),
-                        SizedBox(
-                          height: 80,
-                          child: ListView.builder(
-                              shrinkWrap: true,
-                              itemCount: 6,
-                              scrollDirection: Axis.horizontal,
-                              itemBuilder: (_, index) {
-                                return VerticalImageText(
-                                  image: TImages.shoeIcon,
-                                  title: 'Shoes',
-                                  onTap: () {},
-                                );
-                              }),
-                        ),
+                        ///heading
+                        SectionHeading(
+                            title: 'Popular Categories',
+                            showActionButton: false,
+                            textColor: TColors.white),
+                        SizedBox(height: TSizes.spaceBtwItems),
+
+                        ///categories
+                        HomeCategories(),
                       ],
                     ),
                   ),
                 ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class VerticalImageText extends StatelessWidget {
-  const VerticalImageText({
-    super.key,
-    required this.image,
-    required this.title,
-    this.textColor = TColors.white,
-    this.backgroundColor = TColors.white,
-    this.onTap,
-  });
-
-  final String image, title;
-  final Color textColor;
-  final Color? backgroundColor;
-  final void Function()? onTap;
-
-  @override
-  Widget build(BuildContext context) {
-    final dark = THelperFunctions.isDarkMode(context);
-    return GestureDetector(
-      onTap: onTap,
-      child: Padding(
-        padding: const EdgeInsets.only(right: TSizes.spaceBtwItems),
-        child: Column(
-          children: [
-            /// circiular icon
-            Container(
-              width: 56,
-              height: 56,
-              padding: const EdgeInsets.all(TSizes.sm),
-              decoration: BoxDecoration(
-                color:
-                    backgroundColor ?? (dark ? TColors.black : TColors.white),
-                borderRadius: BorderRadius.circular(100),
-              ),
-              child: Center(
-                child: Image(
-                  image: AssetImage(image),
-                  fit: BoxFit.cover,
-                  color: dark ? TColors.light : TColors.dark,
-                ),
-              ),
-            ),
-            const SizedBox(height: TSizes.spaceBtwItems / 2),
-            SizedBox(
-              width: 55,
-              child: Text(
-                title,
-                style: Theme.of(context)
-                    .textTheme
-                    .labelMedium!
-                    .apply(color: textColor),
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
               ),
             ),
           ],
